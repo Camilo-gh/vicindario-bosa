9dba998f7eeef68256bf29604bed6786cbd6283c
JavierPulido <pjavierfelipe@hotmail.com>
2025-09-03 20:50:29 -0500

    Primer commit del proyecto vecindarioBosa

---
+++ src/main/kotlin/com/example/vecindarioBosa/VecindarioBosaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vecindarioBosa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VecindarioBosaApplication
+
+fun main(args: Array<String>) {
+	runApplication<VecindarioBosaApplication>(*args)
+}
